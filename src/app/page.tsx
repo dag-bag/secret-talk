1cc25d4c37c4df8f9f6329587e4e833b6e049ece
dag-bag <88625252+dag-bag@users.noreply.github.com>
2024-09-16 22:34:02 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { MultilingualConnectComponent } from "@/components/multilingual-connect";
-import Image from "next/image";
 
 export default function Home() {
   return <MultilingualConnectComponent />;
